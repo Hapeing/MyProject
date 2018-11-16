ce1d1cdfcbc240a86b41ae914a1f0e25cb55cda8
Hapeing <hampushallstrom@hotmail.com>
2018-11-16 23:57:59 +0100

    first add

---
+++ Intermediate/Build/Win64/MyProjectEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Hampus/Documents/Unreal Projects/MyProject/Intermediate/Build/Win64/MyProjectEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ Source/MyProject/MyActor.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class MYPROJECT_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+	int32 TotalDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float DamageTimeInSeconds;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Transient, Category = "Damage")//transient means that it is a non-persistent value, it is meant to change
+		float DamagePerSecond;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+};
